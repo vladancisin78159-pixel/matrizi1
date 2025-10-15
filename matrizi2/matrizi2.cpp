60ebbd07fc72291df6270309193ec898280ae3b5
vladancisin78159-pixel <vladancisin78159@gmail.com>
2025-10-15 14:59:31 +0300

    matrizi2.cpp

--- matrizi2/matrizi2.cpp
+++ matrizi2/matrizi2.cpp
@@ -1,4 +1,4 @@
-﻿
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -10,10 +10,9 @@ int main() {
     int matrix[N][M];
 
     // Инициализация генератора случайных чисел
-    srand(time(NULL));
+    srand((unsigned)time(NULL));
 
     // Заполнение матрицы случайными числами
-    printf("Матрица %dx%d:\n", N, M);
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             matrix[i][j] = rand() % 100; // числа от 0 до 99
@@ -24,23 +23,14 @@ int main() {
 
     // Поиск максимального элемента
     int max_value = matrix[0][0];
-    int max_row = 0;
-    int max_col = 0;
-
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             if (matrix[i][j] > max_value) {
                 max_value = matrix[i][j];
-                max_row = i;
-                max_col = j;
             }
         }
     }
-
     // Вывод результата
     printf("\nМаксимальный элемент: %d\n", max_value);
-    printf("Позиция: строка %d, столбец %d\n", max_row + 1, max_col + 1);
-    printf("(индексы: [%d][%d])\n", max_row, max_col);
-
     return 0;
 }
