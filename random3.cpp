4b28a47b5f4ba4b4e9597a0edd2abd3539e19633
vladancisin78159-pixel <vladancisin78159@gmail.com>
2025-11-05 22:22:18 +0300

    Add files via upload

---
+++ random3.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+
+#define SIZE 5
+
+int main() {
+    int matrix[SIZE][SIZE];
+    int value = 1;
+
+    int verxstroka = 0, nizstroka = SIZE - 1;
+    int left = 0, right = SIZE - 1;
+
+    // Заполнение матрицы по спирали
+    while (verxstroka <= nizstroka && left <= right) {
+        // Верхняя строка: слева направо
+        for (int i = left; i <= right; i++) {
+            matrix[verxstroka][i] = value++;
+        }
+        verxstroka++;
+
+        // Правый столбец: сверху вниз
+        for (int i = verxstroka; i <= nizstroka; i++) {
+            matrix[i][right] = value++;
+        }
+        right--;
+
+        // Нижняя строка: справа налево
+        if (verxstroka <= nizstroka) {
+            for (int i = right; i >= left; i--) {
+                matrix[nizstroka][i] = value++;
+            }
+            nizstroka--;
+        }
+
+        // Левый столбец: снизу вверх
+        if (left <= right) {
+            for (int i = nizstroka; i >= verxstroka; i--) {
+                matrix[i][left] = value++;
+            }
+            left++;
+        }
+    }
+
+    // Вывод матрицы, заполненной по спирали
+    printf("Результат:\n");
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("%3d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ random4.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+void pascal(int n) {
+
+    int** shaurma = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        shaurma[i] = (int*)malloc((i + 1) * sizeof(int));
+    }
+
+    for (int i = 0; i < n; i++) {
+        //  пробелы для красивого вывода(ну тип красиво будет)
+        for (int space = 0; space < n - i - 1; space++) {
+            printf(" ");
+        }
+
+        for (int j = 0; j <= i; j++) {
+            if (j == 0 || j == i) {
+                shaurma[i][j] = 1;
+            }
+            else {
+                shaurma[i][j] = shaurma[i - 1][j - 1] + shaurma[i - 1][j];
+            }
+            printf("%d ", shaurma[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < n; i++) {
+        free(shaurma[i]);
+    }
+    free(shaurma);
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int stroki;
+    printf("Введите количество строк треугольника Паскаля: ");
+    scanf_s("%d", &stroki);
+
+    pascal(stroki);
+
+    return 0;
+}
